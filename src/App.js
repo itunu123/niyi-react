7f308dae30690e71213721b9fd5d7ea7283f84f1
Gopi <59192600+GopiPacha1996@users.noreply.github.com>
2022-03-02 13:57:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to CI/CD happt Diwali **###
+          Welcome React app Dev Version
         </p>
         <a
           className="App-link"
