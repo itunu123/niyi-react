d5bb8c8ecb91ffecf300c4288d5a4c9874c83e59
Gopi <59192600+GopiPacha1996@users.noreply.github.com>
2022-03-11 14:18:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Welcome to Enlight DevOps learn build and Deploy
+          HeY A small change will change your destiny
         </p>
         <a
           className="App-link"
